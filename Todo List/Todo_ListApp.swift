9efb4e4ecf551014624e462d13835f8b775fafb9
Haris Alicic <haris.alicic1@gmail.com>
2025-01-19 16:30:39 +0100

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Haris Alicic on 19. 1. 2025..
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
